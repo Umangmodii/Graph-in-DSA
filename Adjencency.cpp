970b7b71b5d26678203287ea9dee7006335a3307
umangmodii <Umangmodi003@gmail.com>
2024-02-06 23:48:03 +0530

    commit

---
+++ Adjencency.cpp
@@ -0,0 +1,80 @@
+// Graph in Adcency List
+
+#include <iostream>
+#include <unordered_map>
+#include <list>
+
+using namespace std;
+
+// Create a class
+class graph
+{
+public:
+    unordered_map<int, list<int>> adj;
+
+    void addEdge(int u, int v, bool direction)
+    {
+        // Direction = 0 -> Undirected Graph
+        // Direction = 1 -> Directed Graph
+
+        adj[u].push_back(v);
+
+        // Check Direction;
+
+        if (direction == 0)
+        {
+            adj[v].push_back(u);
+        }
+    }
+
+    // Print Adj List
+
+    void printAdjList()
+    {
+        for (auto i : adj)
+        {
+            cout << i.first << " -> ";
+
+            for (auto j : i.second)
+            {
+                cout << j << ", ";
+            }
+
+            cout << endl;
+        }
+    }
+};
+
+// Create a Unordered_map
+
+int main()
+{
+    int n;
+
+    cout << "Enter the Number of Nodes : " << endl;
+    cin >> n;
+
+    int m;
+
+    cout << "Enter the Number of Edges : " << endl;
+    cin >> m;
+
+    // Create a Graph
+    graph g;
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+
+        cin >> u >> v;
+
+        // Creating an Undireced Graph
+
+        g.addEdge(u, v, 0);
+    }
+
+    // Printing graph
+    g.printAdjList();
+
+    return 0;
+}
